26f64322cfbbaee9ea496f2711bb3411fcce2e9c
Mad Man Mo <140839348+MadManMo@users.noreply.github.com>
2026-03-30 15:24:15 -0400

    Update NixOS configuration with new settings

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,17 +1,50 @@
-{ config, pkgs, lib, ... }: {
+{ config, pkgs, lib, inputs, ... }: {
   imports = [
     ./hardware-configuration.nix
-    ./modules/default.nix  # your custom modules
   ];
 
-  networking.hostName = "nixos";
-  time.timeZone = "Etc/UTC";  # change to your TZ
+  networking.hostName = "hyprixzero";
+  time.timeZone = "America/New_York";
   i18n.defaultLocale = "en_US.UTF-8";
 
   users.users.dairozero = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" "video" "audio" ];
-    shell = pkgs.fish;  # Caelestia uses fish
+    shell = pkgs.fish;
+  };
+
+  boot = {
+    kernelPackages = pkgs.linuxPackages_zen;
+    kernelModules = [ "v4l2loopback" ];
+    extraModulePackages = [ config.boot.kernelPackages.v4l2loopback ];
+    kernel.sysctl = {
+      "vm.max_map_count" = 2147483642;
+    };
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+    binfmt.registrations.appimage = {
+      wrapInterpreterInShell = false;
+      interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+      recognitionType = "magic";
+      offset = 0;
+      mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+      magicOrExtension = ''\x7fELF....AI\x02'';
+    };
+    plymouth.enable = true;
+  };
+
+  nix = {
+    settings = {
+      auto-optimise-store = true;
+      experimental-features = "nix-command flakes";
+      flake-registry = "";
+      nix-path = config.nix.nixPath;
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
   };
 
   hardware.nvidia = {
@@ -28,15 +61,15 @@
     QT_QPA_PLATFORM = "wayland";
   };
 
-  programs.hyprland.enable = true;  # base enable (Caelestia will configure it)
+  programs.hyprland.enable = true;
   services.pipewire = { enable = true; alsa.enable = true; pulse.enable = true; };
 
   fonts.packages = with pkgs; [
-  pkgs.nerd-fonts.fira-code
-  pkgs.nerd-fonts.jetbrains-mono
-  pkgs.nerd-fonts.symbols-only
-  pkgs.nerd-fonts.iosevka-term
-  pkgs.nerd-fonts.iosevka-term-slab
+    pkgs.nerd-fonts.fira-code
+    pkgs.nerd-fonts.jetbrains-mono
+    pkgs.nerd-fonts.symbols-only
+    pkgs.nerd-fonts.iosevka-term
+    pkgs.nerd-fonts.iosevka-term-slab
   ];
 
   services.flatpak.enable = true;
