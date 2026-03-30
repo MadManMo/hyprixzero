71f699028d9bec471b6abd7dd1d532d7ac19a6d7
Mad Man Mo <140839348+MadManMo@users.noreply.github.com>
2026-03-30 13:12:45 -0400

    Add Flatpak service and flathub repository setup
    Enable Flatpak service and configure flathub repository.

--- modules/nixos/flatpack.nix
+++ modules/nixos/flatpack.nix
@@ -1,6 +1,10 @@
-# Add your reusable home-manager modules to this directory, on their own file (https://nixos.wiki/wiki/Module).
-# These should be stuff you would like to share with others, not your personal configurations.
-{
-  # List your module files here
-  # my-module = import ./my-module.nix;
+{ ... }: {
+  services.flatpak.enable = true;
+  # Optional: auto-add flathub on first rebuild
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    script = ''
+      ${pkgs.flatpak}/bin/flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
 }
