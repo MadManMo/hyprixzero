58a43f2b5eac45f24dbdd82e74a5cbaf9568b11e
Mad Man Mo <140839348+MadManMo@users.noreply.github.com>
2026-03-30 21:31:17 -0400

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -4,6 +4,8 @@
     #./pkgs/default.nix
   ];
 
+  nixpkgs.config.allowUnfree = true;
+
   networking.hostName = "hyprixzero";
   time.timeZone = "America/New_York";
   i18n.defaultLocale = "en_US.UTF-8";
