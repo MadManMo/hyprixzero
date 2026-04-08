621a04a03475ca23126e5dd74f6af49544c91317
Mad Man Mo <140839348+MadManMo@users.noreply.github.com>
2026-04-07 20:45:40 -0400

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -39,6 +39,7 @@
 
   # ── Nix ──────────────────────────────────────
   nix = {
+    package = pkgs.nix;
     settings = {
       auto-optimise-store = true;
       experimental-features = "nix-command flakes";
